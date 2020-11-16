7102cf861312fe067a3c5b1418e84522de181343
JUBY VARUGHESE <juby@bcinnovationlabs.com>
2020-11-16 17:31:52 -0500

    Updated SideBar Navigations

--- src/components/medicationLogsComponent.tsx
+++ src/components/medicationLogsComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThLarge,faUser,faBalanceScale,faHistory,faShareAlt,faSignOutAlt,faQuestionCircle, faUserAlt } from '@fortawesome/free-solid-svg-icons';
+import { faThLarge,faUser,faBalanceScale,faHistory,faShareAlt,faSignOutAlt,faQuestionCircle, faUserAlt,faRuler } from '@fortawesome/free-solid-svg-icons';
 import { Container,Image,Table } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import {
@@ -35,6 +35,7 @@ const MedicationLogsComponent = (props: IProps) : JSX.Element => {
                         <Image className="menu rounded img-fluid mb-5" style={{display: "block",marginLeft: "auto",marginRight: "auto"}} src={logo} width="120rem" height="100rem"/>
                         <Link id="dashboard" className="menu ml-1 p-2" to="/dashboard"><FontAwesomeIcon icon={faThLarge} />&nbsp;&nbsp;&nbsp;Dashboard</Link>
                         <Link id="profile" className="menu ml-1 p-2" to="/user-profile"><FontAwesomeIcon icon={faUser} />&nbsp;&nbsp;&nbsp;Account</Link>
+                        <Link id="measure" className="menu p-2" to="/measurement"><FontAwesomeIcon icon={faRuler} />&nbsp;&nbsp;Measurement</Link>
                         <Link id="health-check" className="menu p-2" to="/health-check"><FontAwesomeIcon icon={faBalanceScale} />&nbsp;&nbsp;Health Check</Link>
                         <Link id="medication-history" className="menu ml-1 p-2" to="/medication-logs"><FontAwesomeIcon icon={faHistory} />&nbsp;&nbsp;Medication logs</Link>
                         <Link id="share" className="menu ml-1 p-2" to="/"><FontAwesomeIcon icon={faShareAlt} />&nbsp;&nbsp;Share</Link>
--- src/components/userProfileComponent.tsx
+++ src/components/userProfileComponent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Row,Col,Card,Container,Table,Image,Form, Button } from 'react-bootstrap';
 import { slide as Menu } from 'react-burger-menu';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThLarge,faUser,faBalanceScale,faHistory,faShareAlt,faSignOutAlt,faMapMarkerAlt,faBriefcase,faUserAlt,faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { faThLarge,faUser,faBalanceScale,faHistory,faShareAlt,faSignOutAlt,faMapMarkerAlt,faBriefcase,faUserAlt,faQuestionCircle, faRuler } from '@fortawesome/free-solid-svg-icons';
 import {
     DropdownMenu,
     DropdownItem,
@@ -34,6 +34,7 @@ const UserProfileComponent = (props: IProps) : JSX.Element => {
                 <Image className="menu rounded img-fluid mb-5" style={{display: "block",marginLeft: "auto",marginRight: "auto"}} src={logo} width="120rem" height="100rem"/>
                 <Link id="dashboard" className="menu ml-1 p-2" to="/dashboard"><FontAwesomeIcon icon={faThLarge} />&nbsp;&nbsp;&nbsp;Dashboard</Link>
                 <Link id="profile" className="menu ml-1 p-2" to="/user-profile"><FontAwesomeIcon icon={faUser} />&nbsp;&nbsp;&nbsp;Account</Link>
+                <Link id="measure" className="menu p-2" to="/measurement"><FontAwesomeIcon icon={faRuler} />&nbsp;&nbsp;Measurement</Link>
                 <Link id="health-check" className="menu p-2" to="/health-check"><FontAwesomeIcon icon={faBalanceScale} />&nbsp;&nbsp;Health Check</Link>
                 <Link id="medication-history" className="menu ml-1 p-2" to="/medication-logs"><FontAwesomeIcon icon={faHistory} />&nbsp;&nbsp;Medication logs</Link>
                 <Link id="share" className="menu ml-1 p-2" to="/"><FontAwesomeIcon icon={faShareAlt} />&nbsp;&nbsp;Share</Link>
