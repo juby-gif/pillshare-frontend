da8aac3ec57fd789547ee14adbd316e1c53a3f12
JUBY VARUGHESE <juby@bcinnovationlabs.com>
2020-11-21 22:44:35 -0500

    Added Login capability with API connections and fixed vital measurements

--- src/containers/loginContainer.tsx
+++ src/containers/loginContainer.tsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { RouteComponentProps  } from 'react-router';
 
 import LoginComponent from '../components/loginComponent';
-import { BIODB_TOKEN,BIODB_LOGGED_IN_USER } from '../constants';
 import { postLogin } from "../API/loginAPI"
 import '../App.css'
 
@@ -24,116 +23,116 @@ interface ResponseProps {
 }
 export default class LoginContainer extends Component<IProps & RouteComponentProps,StateProps> {
 
-      /*   *
-           *  Initializer
-           *------------------------------------------------------------
-      */
-      constructor(props:IProps & RouteComponentProps){
+    /*   *
+        *  Initializer
+        *------------------------------------------------------------
+    */
+    constructor(props:IProps & RouteComponentProps){
         super(props);
         this.state={
-          username:"",
-          password:"",
-          response:"",
-          message:"",
-          validated:false,
-        }
-        this.onUsernameChange = this.onUsernameChange.bind(this);
-        this.onPasswordChange = this.onPasswordChange.bind(this);
-        this.onLoginClick = this.onLoginClick.bind(this);
-        this.onRegisterClick = this.onRegisterClick.bind(this);
-        this.onSuccessCallBack = this.onSuccessCallBack.bind(this);
-        this.onFailureCallBack = this.onFailureCallBack.bind(this);
-      }
+            username:"",
+            password:"",
+            response:"",
+            message:"",
+            validated:false,
+    }
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onLoginClick = this.onLoginClick.bind(this);
+    this.onRegisterClick = this.onRegisterClick.bind(this);
+    this.onSuccessCallBack = this.onSuccessCallBack.bind(this);
+    this.onFailureCallBack = this.onFailureCallBack.bind(this);
+    }
 
-      /* *
-         *  Utility
-         *------------------------------------------------------------
-      */
-      //Nothing
+    /* *
+        *  Utility
+        *------------------------------------------------------------
+    */
+    //Nothing
 
-      /* *
-         *  Component Life-cycle Management
-         *------------------------------------------------------------
-      */
-      //Nothing
+    /* *
+        *  Component Life-cycle Management
+        *------------------------------------------------------------
+    */
+    //Nothing
 
-      /* *
-         *  API callback functions
-         *------------------------------------------------------------
-      */
-      onLoginAPIProcessCall = (username:string,password:string):void => {
+    /* *
+        *  API callback functions
+        *------------------------------------------------------------
+    */
+    onLoginAPIProcessCall = (username:string,password:string):void => {
         const { onSuccessCallBack,onFailureCallBack } = this;
         const postData = {
             username: username,
             password: password,
         };
-        postLogin(postData, onSuccessCallBack, onFailureCallBack);
-      }
+    postLogin(postData, onSuccessCallBack, onFailureCallBack);
+    }
 
-      /* *
-         *  Event handling functions
-         *------------------------------------------------------------
-      */
-      onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    /* *
+        *  Event handling functions
+        *------------------------------------------------------------
+    */
+    onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
         this.setState({
-          message:""
+            message:""
         })
         this.setState({
-          username:event.currentTarget.value,
+            username:event.currentTarget.value,
         })
-      }
+    }
 
-      onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
         this.setState({
-          message:"",
+            message:"",
         })
         this.setState({
-          password:event.currentTarget.value,
+            password:event.currentTarget.value,
         })
-      }
+    }
 
-      onLoginClick = (event: React.SyntheticEvent): void => {
+    onLoginClick = (event: React.SyntheticEvent): void => {
         event.preventDefault();
         const {username,password} = this.state;
         this.onLoginAPIProcessCall(username,password);
-      }
+    }
 
-      onRegisterClick = (event: React.SyntheticEvent): void => {
+    onRegisterClick = (event: React.SyntheticEvent): void => {
         event.preventDefault();
         this.props.history.push("/register")
-      }
+    }
 
-      onSuccessCallBack = (responseData: ResponseProps): void => {
-          console.log("Messege => ",responseData.message,"Token => ",responseData.token);
-          this.props.history.push("/dashboard");
+    onSuccessCallBack = (responseData: ResponseProps): void => {
+        console.log("Messege => ",responseData.message,"Token => ",responseData.token);
+        this.props.history.push("/dashboard");
 
-      }
-      
-      onFailureCallBack = (responseData: ResponseProps): void => {
+    }
+    
+    onFailureCallBack = (responseData: ResponseProps): void => {
         alert(responseData.message);
-      }
+    }
 
-      /* *
-         *  Main render function
-         *------------------------------------------------------------
-      */
-      render(){
+    /* *
+        *  Main render function
+        *------------------------------------------------------------
+    */
+    render(){
         const {username,password,response,message,validated} = this.state;
         const {onUsernameChange,onPasswordChange,onLoginClick,onRegisterClick} = this;
         return(
-          <div>
-              <LoginComponent
-              username={username}
-              password={password}
-              response={response}
-              message={message}
-              validated={validated}
-              onUsernameChange={onUsernameChange}
-              onPasswordChange={onPasswordChange}
-              onLoginClick={onLoginClick}
-              onRegisterClick={onRegisterClick}
-              />
-          </div>
-        );
-      }
+            <div>
+                <LoginComponent
+                username={username}
+                password={password}
+                response={response}
+                message={message}
+                validated={validated}
+                onUsernameChange={onUsernameChange}
+                onPasswordChange={onPasswordChange}
+                onLoginClick={onLoginClick}
+                onRegisterClick={onRegisterClick}
+                />
+            </div>
+            );
+        }
 }
