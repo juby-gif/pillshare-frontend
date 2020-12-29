d3f04827008940bf6633753890dffa7b7b0b48b0
JUBY VARUGHESE <juby@bcinnovationlabs.com>
2020-12-29 16:25:32 -0500

    Bugfix on Client side Graph loader

--- src/containers/viewMoreContainers/glucoseContainer.tsx
+++ src/containers/viewMoreContainers/glucoseContainer.tsx
@@ -73,151 +73,164 @@ export default class GlucoseViewMoreContainer extends Component<IProps,StateProp
             // Added date component only for plotting
             graphData.push({date:moment.unix(timestamp).utcOffset('-0000').format("YYYY-MM-DD HH:mm"),value:datum.reading})
         }
+    
         let chart:any = am4core.create("chartdiv", am4charts.XYChart);
         chart.preloader.disabled = true;
         chart.responsive.enabled = true;
-        chart.events.on('ready', () => {
-            hideIndicator();
-          });
-        chart.events.on("beforedatavalidated", function(ev:any) {
-        // check if there's data
-        if (ev.target.data.length === 0) {
-            showIndicator();
-        }
-        else if (indicator) {
-            hideIndicator();
-        }
-        });
-        if(graphData.length !== 0){
-        chart.data = graphData;
-        }
         
-        let indicator:any;
-        let indicatorInterval:any;
-        const showIndicator = ():void => {
-    
-            if (indicator) {
+        chart.events.on("beforedatavalidated", function() {
+            if(chart.data.length === 0){
+                setTimeout(function() {
+                    hideIndicator();
+                    empty();     
+                }, 2000);
+            } else {
+                setTimeout(function() {
+                    hideIndicator();
+                }, 2000);
+            }
+        });
+
+        if(graphData.length !== 0){
+            chart.data = graphData;
+        } else{
+            chart.data = [];
+        }
+        var indicator: any;
+        var indicatorInterval: any;
+
+        function empty () {
+            indicator = chart.tooltipContainer.createChild(am4core.Container);
+            indicator.background.fill = am4core.color("#fff");
+            indicator.background.fillOpacity = 0.8;
+            indicator.width = am4core.percent(100);
+            indicator.height = am4core.percent(100);
+
+            var indicatorLabel = indicator.createChild(am4core.Label);
+            indicatorLabel.text = "No data...";
+            indicatorLabel.align = "center";
+            indicatorLabel.valign = "middle";
+            indicatorLabel.fontSize = 20; 
+        }
+        function showIndicator() {
+            if (!indicator) {
                 indicator = chart.tooltipContainer.createChild(am4core.Container);
                 indicator.background.fill = am4core.color("#fff");
                 indicator.background.fillOpacity = 0.8;
                 indicator.width = am4core.percent(100);
                 indicator.height = am4core.percent(100);
 
-                let indicatorLabel = indicator.createChild(am4core.Label);
+                var indicatorLabel = indicator.createChild(am4core.Label);
                 indicatorLabel.text = "Loading Graph...";
                 indicatorLabel.align = "center";
                 indicatorLabel.valign = "middle";
                 indicatorLabel.fontSize = 20;
                 indicatorLabel.dy = 50;
                 
-                let hourglass = indicator.createChild(am4core.Image);
+                var hourglass = indicator.createChild(am4core.Image);
                 hourglass.href = spinner;
                 hourglass.align = "center";
                 hourglass.valign = "middle";
                 hourglass.horizontalCenter = "middle";
                 hourglass.verticalCenter = "middle";
                 hourglass.scale = 0.7;
-
-                clearInterval(indicatorInterval);
-                indicatorInterval = setInterval(function() {
-                hourglass.animate([{
-                    from: 0,
-                    to: 360,
-                    property: "rotation"
-                }], 2000);
-                }, 3000);
-            }  else {
-                indicator = chart.tooltipContainer.createChild(am4core.Container);
-                indicator.background.fill = am4core.color("#fff");
-                indicator.background.fillOpacity = 0.8;
-                indicator.width = am4core.percent(100);
-                indicator.height = am4core.percent(100);
-            
-                var indicatorLabel = indicator.createChild(am4core.Label);
-                indicatorLabel.text = "No data...";
-                indicatorLabel.align = "center";
-                indicatorLabel.valign = "middle";
-                indicatorLabel.fontSize = 20;
-            }  
-        indicator.hide(0);
-        indicator.show();
+                
+                setInterval(function() {
+                    hourglass.animate([{
+                        from: 0,
+                        to: 360,
+                        property: "rotation",
+                    }], 1200);
+                }, 600);
+            }
+            indicator?.hide(0);
+            indicator?.show();
         }
 
-        const hideIndicator = ():void => {
-        indicator.hide();
+        function hideIndicator() {
+        indicator?.hide();
         clearInterval(indicatorInterval);
         }
         showIndicator();
 
+        if(chart.data.length !== 0){
+            // Create axes
+            let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+            dateAxis.renderer.minGridDistance = 60;
+            dateAxis.title.text = "Day";
+            dateAxis.title.fontWeight = "bold";
+            let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+            valueAxis.title.text = "Glucose (mg/dL)";
+            valueAxis.title.fontWeight = "bold";
+            
+            // Create series
+            let series = chart.series.push(new am4charts.LineSeries());
+            series.dataFields.valueY = "value";
+            series.dataFields.dateX = "date";
+            series.tooltipText = "{value}"
 
-        // Create axes
-        let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-        dateAxis.renderer.minGridDistance = 60;
-        dateAxis.title.text = "Day";
-        dateAxis.title.fontWeight = "bold";
-        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-        valueAxis.title.text = "Glucose (mg/dL)";
-        valueAxis.title.fontWeight = "bold";
-        
-        // Create series
-        let series = chart.series.push(new am4charts.LineSeries());
-        series.dataFields.valueY = "value";
-        series.dataFields.dateX = "date";
-        series.tooltipText = "{value}"
+            valueAxis.renderer.minGridDistance = 30;
+            valueAxis.paddingTop = 35;
 
-        valueAxis.renderer.minGridDistance = 30;
-        valueAxis.paddingTop = 35;
+            var range = valueAxis.axisRanges.create();
+            range.value = Math.min(...readingData).valueOf();
+            range.grid.stroke = am4core.color("#3f00d1");
+            range.grid.strokeWidth = 2;
+            range.grid.strokeOpacity = 0.6;
+            range.label.inside = true;
+            range.label.text = "Low: " + Math.min(...readingData).valueOf() + "mg/dL";
+            range.label.fill = range.grid.stroke;
+            range.label.align = "left";
+            range.label.verticalCenter = "bottom";
 
-        var range = valueAxis.axisRanges.create();
-        range.value = Math.min(...readingData).valueOf();
-        range.grid.stroke = am4core.color("#3f00d1");
-        range.grid.strokeWidth = 2;
-        range.grid.strokeOpacity = 0.6;
-        range.label.inside = true;
-        range.label.text = "Low: " + Math.min(...readingData).valueOf() + "mg/dL";
-        range.label.fill = range.grid.stroke;
-        range.label.align = "left";
-        range.label.verticalCenter = "bottom";
+            var range1 = valueAxis.axisRanges.create();
+            range1.value = this.getAverage(readingData)?.toFixed(2);
+            range1.grid.stroke = am4core.color("#6b6d0b");
+            range1.grid.strokeWidth = 2;
+            range1.grid.strokeOpacity = 0.6;
+            range1.label.inside = true;
+            range1.label.text = "Average: " + this.getAverage(readingData)?.valueOf().toPrecision(5) + "mg/dL";
+            range1.label.fill = range1.grid.stroke;
+            range1.label.align = "left";
+            range1.label.verticalCenter = "bottom";
+            
+            var range2 = valueAxis.axisRanges.create();
+            range2.value = Math.max(...readingData).valueOf();
+            range2.grid.stroke = am4core.color("rgb(102,0,0)");
+            range2.grid.strokeWidth = 2;
+            range2.grid.strokeOpacity = 0.6;
+            range2.label.inside = true;
+            range2.label.paddingTop = 4;
+            range2.label.text = "High: " + Math.max(...readingData).valueOf() + "mg/dL"
+            range2.label.fill = range2.grid.stroke;
+            range2.label.align = "left";
+            range2.label.verticalCenter = "bottom";
 
-        var range1 = valueAxis.axisRanges.create();
-        range1.value = this.getAverage(readingData)?.toFixed(2);
-        range1.grid.stroke = am4core.color("#6b6d0b");
-        range1.grid.strokeWidth = 2;
-        range1.grid.strokeOpacity = 0.6;
-        range1.label.inside = true;
-        range1.label.text = "Average: " + this.getAverage(readingData)?.valueOf().toPrecision(5) + "mg/dL";
-        range1.label.fill = range1.grid.stroke;
-        range1.label.align = "left";
-        range1.label.verticalCenter = "bottom";
-        
-        var range2 = valueAxis.axisRanges.create();
-        range2.value = Math.max(...readingData).valueOf();
-        range2.grid.stroke = am4core.color("rgb(102,0,0)");
-        range2.grid.strokeWidth = 2;
-        range2.grid.strokeOpacity = 0.6;
-        range2.label.inside = true;
-        range2.label.paddingTop = 4;
-        range2.label.text = "High: " + Math.max(...readingData).valueOf() + "mg/dL"
-        range2.label.fill = range2.grid.stroke;
-        range2.label.align = "left";
-        range2.label.verticalCenter = "bottom";
-
-        chart.cursor = new am4charts.XYCursor();
-        chart.cursor.snapToSeries = series;
-        chart.cursor.xAxis = dateAxis;
-        
-        // chart.scrollbarY = new am4core.Scrollbar();
-        chart.scrollbarX = new am4core.Scrollbar();
-        
-        this.setState({
-            min:(Math.min(...readingData).valueOf()) === 1/0?0:Math.min(...readingData).valueOf(),
-            max:(Math.max(...readingData).valueOf()) === -1/0?0:Math.max(...readingData).valueOf(),
-            avg:(this.getAverage(readingData)?.valueOf().toPrecision(5)) === undefined?0:this.getAverage(readingData)?.valueOf().toPrecision(5),
-        })
+            chart.cursor = new am4charts.XYCursor();
+            chart.cursor.snapToSeries = series;
+            chart.cursor.xAxis = dateAxis;
+            
+            // chart.scrollbarY = new am4core.Scrollbar();
+            chart.scrollbarX = new am4core.Scrollbar();
+            
+            this.setState({
+                min:(Math.min(...readingData).valueOf()) === 1/0?0:Math.min(...readingData).valueOf(),
+                max:(Math.max(...readingData).valueOf()) === -1/0?0:Math.max(...readingData).valueOf(),
+                avg:(this.getAverage(readingData)?.valueOf().toPrecision(5)) === undefined?0:this.getAverage(readingData)?.valueOf().toPrecision(5),
+            })
+        } else {
+            let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+            dateAxis.renderer.minGridDistance = 60;
+            dateAxis.title.text = "Day";
+            dateAxis.title.fontWeight = "bold";
+            let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+            valueAxis.title.text = "Glucose (mg/dL)";
+            valueAxis.title.fontWeight = "bold";
+        }
     }
 
     onFailureCallBack = (responseData: ServerResponse): void => {
-        console.log(responseData);
+        console.error(responseData);
     }
 
     getAverage = (data:Array<number>):number|undefined => {
